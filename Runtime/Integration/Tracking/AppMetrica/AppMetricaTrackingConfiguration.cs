78782844633c4ff2ff679b31338085322eecc560
Scheherazade <anhvn@hapiga.net>
2025-12-23 15:02:32 +0700

    [Project] :: Add AppMetrica iOS id

--- Runtime/Integration/Tracking/AppMetrica/AppMetricaTrackingConfiguration.cs
+++ Runtime/Integration/Tracking/AppMetrica/AppMetricaTrackingConfiguration.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Com.Hapiga.Scheherazade.Common.Integration.Tracking
 {
     [CreateAssetMenu(
-        fileName = "AppMetricaTrackingConfiguration", 
+        fileName = "AppMetricaTrackingConfiguration",
         menuName = "Dev Menu/Integration/AppMetrica Tracking Configuration"
     )]
     public class AppMetricaTrackingConfiguration : ScriptableObject
     {
-        public string ApiKey => apiKey;
+        public string ApiKey
+        {
+            get
+            {
+#if UNITY_ANDROID
+                return androidApiKey;
+#elif UNITY_IOS
+                return iosApiKey;
+#else
+                return string.Empty;
+#endif
+            }
+        }
+
         public float IapMultiplier => iapMultiplier;
 
         [SerializeField]
-        private string apiKey;
+        [FormerlySerializedAs("apiKey")]
+        private string androidApiKey;
+
+        [SerializeField]
+        private string iosApiKey;
 
         [SerializeField]
         private float iapMultiplier;
